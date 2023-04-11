057e7b272a8c1cb645f89e496bf223798942ac36
Reddi Radha <you@example.com>
2023-04-11 12:18:43 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,17 +1,48 @@
 from django.shortcuts import render
 from app.models import *
+
 # Create your views here.
+from django.db.models.functions import Length
+from django.db.models import Q
 def display_topics(request):
     LOT=Topic.objects.all()
     d={'topics':LOT}
-    return render(request,'display_topics.html',d)
+    return render(request,'display_topics.html',context=d)
 
 def display_webpages(request):
     LOW=Webpage.objects.all()
+    LOW=Webpage.objects.filter(topic_name='cricket')
+    LOW=Webpage.objects.get(topic_name='chess')
+    LOW=Webpage.objects.exclude(topic_name='cricket')
+    LOW=Webpage.objects.all()[1:6]
+    LOW=Webpage.objects.all().order_by('name')
+    LOW=Webpage.objects.all().order_by('-name')
+    
+    LOW=Webpage.objects.all().order_by(Length('name'))
+    LOW=Webpage.objects.all().order_by(Length('name').desc())
+    LOW=Webpage.objects.all()
+    LOW=Webpage.objects.filter(name__startswith='r')
+    LOW=Webpage.objects.filter(email__endswith='.com')
+    LOW=Webpage.objects.filter(name__contains='S')
+    LOW=Webpage.objects.filter(name__in=('msd','rahul'))
+    LOW=Webpage.objects.filter(name__regex='[a-zA-Z]{5}')
+    LOW=Webpage.objects.filter(Q(topic_name='cricket') & Q(name='msd'))
+    LOW=Webpage.objects.filter(Q(topic_name='cricket'))
     d={'webpages':LOW}
     return render(request,'display_webpages.html',d)
 
+
 def display_access(request):
     LOA=AccessRecord.objects.all()
+    LOA=AccessRecord.objects.filter(date__gt='1993-01-10')
+    LOA=AccessRecord.objects.filter(date__gte='2001-10-10')
+    LOA=AccessRecord.objects.filter(date__lt='2000-10-10')
+    LOA=AccessRecord.objects.filter(date__lte='2002-10-10')
+    LOA=AccessRecord.objects.filter(date__year='2001')
+    LOA=AccessRecord.objects.filter(date__month='11')
+    LOA=AccessRecord.objects.filter(date__day='23')
+    LOA=AccessRecord.objects.filter(date__year__gt='2000')
+    LOA=AccessRecord.objects.filter(date__month__lt='10')
+    
     d={'access':LOA}
     return render(request,'display_access.html',d)
